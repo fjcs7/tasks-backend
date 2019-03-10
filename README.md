2e91e331390390d4492c0ee0439698530425f225
Fernando Junio Cunha e Sousa <fjcs7@hotmail.com>
2019-03-10 03:00:23 -0300

    Commit do projeto e das migrations iniciais

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taks Backend
+Projeto criado para estudos relacionados ao react-native
+-Este projeto consiste em implementar uma aplicação do tipo gerenciador de tarefas que funcione tanto no Android, quando iOS, esta parte do projeto consiste na criação da API que irá fornecer os serviços para os APPs.
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consing = require('consign')
+
+consing()
+    .then('./config/middlewares.js')
+    .into(app)
+
+app.db = db
+
+
+app.listen(3000, ()=>{
+    console.log('Backend em execução na porta 3000...')
+})
